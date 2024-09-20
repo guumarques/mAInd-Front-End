b5a789ca10f383f369bdbba3fec0f083f6d1f116
Gustavo Silva Marques <110607596+guumarques@users.noreply.github.com>
2024-09-20 18:07:45 -0300

    Update mensagem.js

--- src/js/mensagem.js
+++ src/js/mensagem.js
@@ -112,7 +112,7 @@ let urlAPI = 'https://web-production-11e8.up.railway.app/mAInd';
 let urlTeste = 'http://127.0.0.1:5000/mAInd';
 // Função para iniciar a conversa e obter o user_id
 function iniciarConversa() {
-    fetch('${urlAPI}/start', {
+    fetch(`${urlAPI}/start`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
